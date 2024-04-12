0a98df32219a812a164000978cbf0b05156f5000
anirudhj2000 <anirudhjoshi985@gmail.com>
2024-04-12 17:56:35 +0530

    minor fixes

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { motion } from "framer-motion";
+import RevealOnScroll from "@/components/RevealOnScroll";
 
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between">
+      <div className="absolute z-100 flex flex-col w-full top-0 h-72 bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-[#0097b244] via-[#0097b211] to-[#0097b200] "></div>
+      <div className="absolute z-0 inset-0 h-full to-0 w-full bg-transparent bg-[linear-gradient(to_right,#80808022_1px,transparent_1px),linear-gradient(to_bottom,#80808022_1px,transparent_1px)] bg-[size:24px_24px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_40%,#000_60%,transparent_100%)]"></div>
+    </div>
+  );
 };
 
 export default Services;
--- components/contactus.jsx
+++ components/contactus.jsx
@@ -5,15 +5,12 @@ const section4 = "Contact Us";
 
 const ContactUs = () => {
   return (
-    <RevealOnScroll
-      threshold={0.65}
-      addedClasses={" w-full md:w-10/12 animate-slideUp"}
-    >
+    <RevealOnScroll threshold={0.65} addedClasses={" w-10/12 animate-slideUp"}>
       <section className="flex flex-col items-center w-full relative z-0 ">
-        <div className="flex flex-row w-full h-full relative justify-between items-center my-[10vh] rounded-xl overflow-hidden [border-image:linear-gradient(to_right,#0087B2,#FFFFFF,#043357)_1] border-[1px]">
+        <div className="flex flex-col md:flex-row w-full h-full relative justify-between items-center my-[10vh] rounded-xl overflow-hidden [border-image:linear-gradient(to_right,#0087B2,#FFFFFF,#043357)_1] border-[1px]">
           <div className="absolute inset-0 h-full w-full bg-transparent bg-[linear-gradient(to_right,#80808022_1px,transparent_1px),linear-gradient(to_bottom,#80808022_1px,transparent_1px)] bg-[size:24px_24px]"></div>
 
-          <div className="flex flex-col w-[50%] items-start justify-center px-8">
+          <div className="flex flex-col w-full md:w-[50%] items-start justify-center px-8">
             <h1 className="overflow-hidden text-[3rem] text-start font-bold leading-[3rem] text-[#0097b2]">
               {section4.split("").map((char, index) => (
                 <span
@@ -31,7 +28,7 @@ const ContactUs = () => {
               }
             </p>
           </div>
-          <div className="flex flex-col w-[50%] items-end justify-center px-8 my-[7.5vh]">
+          <div className="flex flex-col w-full md:w-[50%] items-center md:items-end justify-center px-0 md:px-8 my-[7.5vh]">
             <div className="flex flex-col w-10/12 space-y-4 animate-slideUpSmall">
               <div className="flex flex-col items-start">
                 <label className="text-white/80">Name</label>
