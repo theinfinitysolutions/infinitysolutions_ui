19757d5a791b05e68d79c71c5f57cbddafa1cc00
anirudhj2000 <anirudhjoshi985@gmail.com>
2024-04-15 00:42:34 +0530

    consts changes

--- utils/consts.js
+++ utils/consts.js
@@ -189,3 +189,126 @@ export const serviceList = [
     ],
   },
 ];
+
+export const projects = [
+  {
+    title: "Ace Resumes",
+    description:
+      "A platform for creating professional resumes with customizable templates and easy-to-use tools.",
+    image: "/aceresumes.png",
+    type: "Website",
+    industry: "Education",
+    testimonial: {
+      name: "Apoorva Choudhary",
+      designation: "CEO, Ace Resumes",
+      message:
+        "I am very impressed with the quality of work and the dedication of the team. They delivered the project on time and exceeded our expectations. Highly recommended!",
+    },
+    location: "India",
+    link: "http://144.24.111.231:7003",
+    techStack: [
+      {
+        image: "/nextjs.png",
+        title: "NextJS",
+      },
+      {
+        image: "/django.png",
+        title: "Django",
+      },
+      {
+        image: "/postgresql.png",
+        title: "PostgreSQL",
+      },
+    ],
+  },
+  {
+    title: "Mahakali Sarees",
+    description:
+      "An e-commerce website for selling a wide range of traditional Indian sarees.",
+    image: "/mahakali.png",
+    type: "Website",
+    location: "India",
+    industry: "Fashion, E-commerce",
+    testimonial: {
+      name: "Manish Sanghai",
+      designation: "Director, Mahakali Sarees",
+      message:
+        "The team at Infinity Solutions did an amazing job in developing our e-commerce platform. The website is user-friendly, visually appealing, and has helped us boost our online sales.",
+    },
+    link: "https://mahakalisarees.com",
+    techStack: [
+      {
+        image: "/nextjs.png",
+        title: "NextJS",
+      },
+      {
+        image: "/nodejs.png",
+        title: "Nodejs",
+      },
+      {
+        image: "/postgresql.png",
+        title: "PostgreSQL",
+      },
+    ],
+  },
+  {
+    title: "Shree Govind Tourism",
+    description:
+      "A travel agency website for booking tours and travel packages.",
+    image: "/sgt.png",
+    type: "Website",
+    industry: "Travel, Tourism",
+    testimonial: {
+      name: "Bhasker",
+      designation: "Director, Shree Govind Tourism",
+      message:
+        "The team at Infinity Solutions is highly skilled and professional. They developed our website exactly as per our requirements and provided excellent support throughout the project.",
+    },
+    location: "India",
+    link: "https://www.shreegovindtourism.com",
+    techStack: [
+      {
+        image: "/vuejs.png",
+        title: "Vuejs",
+      },
+      {
+        image: "/django.png",
+        title: "Django",
+      },
+      {
+        image: "/postgresql.png",
+        title: "PostgreSQL",
+      },
+    ],
+  },
+  {
+    title: "Raksheet Hospital",
+    description:
+      "A hospital management system for managing patient records, appointments, and medical services.",
+    image: "/raksheet.png",
+    type: "Website",
+    location: "India",
+    testimonial: {
+      name: "Raksheet Jain",
+      designation: "Director, Raksheet Hospital",
+      message:
+        "The team at Infinity Solutions developed a robust and user-friendly hospital management system for us. It has streamlined our operations and improved patient care.",
+    },
+    industry: "Healthcare Services",
+    link: "http://144.24.111.231:7005",
+    techStack: [
+      {
+        image: "/nextjs.png",
+        title: "NextJS",
+      },
+      {
+        image: "/django.png",
+        title: "Django",
+      },
+      {
+        image: "/postgresql.png",
+        title: "PostgreSQL",
+      },
+    ],
+  },
+];
