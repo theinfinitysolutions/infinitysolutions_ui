00c0bcdd4c3d49ed9514bcb149820c320f67f5f8
anirudhj2000 <anirudhjoshi985@gmail.com>
2024-04-22 01:53:49 +0530

    layout and ctsconfig changes

--- app/layout.js
+++ app/layout.js
@@ -1,19 +1,18 @@
-"use client";
 import { Lora } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
 import Cursor from "@/components/cursor";
-import { useEffect } from "react";
 import "normalize.css/normalize.css";
 import Head from "next/head";
 
 const inter = Lora({ subsets: ["latin"] });
 
-// export const metadata = {
-//   title: "The Infinity Solutions",
-//   description: "The Infinity Solutions",
-// };
+export const metadata = {
+  title: "The Infinity Solutions",
+  description:
+    "The Infinity Solutions - Best Tech Consultancy in India - Website,  Mobile App, & Custom Tech Solutions | Data Analysis | Data Mining",
+};
 
 export default function RootLayout({ children }) {
   return (
@@ -25,6 +24,18 @@ export default function RootLayout({ children }) {
           The Infinity Solutions - Best Tech Consultancy in India - Website,
           Mobile App, & Custom Tech Solutions | Data Analysis | Data Mining
         </title>
+        <meta
+          name="description"
+          content="The Infinity Solutions is a tech consultancy in India that provides website, mobile app, and custom tech solutions, data analysis, and data mining services."
+        />
+        <meta property="og:title" content="The Infinity Solutions" />
+        <meta
+          property="og:description"
+          content="The Infinity Solutions is a tech consultancy in India that provides website, mobile app, and custom tech solutions, data analysis, and data mining services."
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://theinfinitysolutions.net" />
+        <meta property="og:image" content="/opengraph-image.png" />
       </Head>
       <body
         className={`${inter.className} bg-black max-w-screen overflow-x-hidden`}
